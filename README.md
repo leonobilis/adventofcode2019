626343a72d63936d3678b7f45eeddcb45dfa6c73
Marcin L <lev7@vp.pl>
2019-12-01 06:45:15 +0100

    day 1

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# adventofcode2019
+# Advent of Code 2019
---
+++ day01/day01.py
@@ -0,0 +1,29 @@
+from math import floor
+
+
+def fuel(mass):
+    return floor(mass/3) - 2
+
+
+def total_fuel(mass):
+    result = 0
+    r = fuel(mass)
+    while r > 0:
+        result += r
+        r = fuel(r)
+    return result
+
+
+def p1(input):
+    return sum([fuel(int(i)) for i in input])
+
+
+def p2(input):
+    return sum([total_fuel(int(i)) for i in input])
+
+
+if __name__ == "__main__":
+    with open('input.txt', "r") as f:
+        inp = f.readlines()
+        print("Part 1: {}".format(p1(inp)))
+        print("Part 2: {}".format(p2(inp)))
