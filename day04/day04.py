d8f53798b07d6e6a48312a5462a46634ef87ac68
Marcin L <lev7@vp.pl>
2019-12-04 07:22:43 +0100

    day 4

---
+++ day04/day04.py
@@ -0,0 +1,24 @@
+from itertools import groupby
+
+def criteria(password):
+    return any([password[i] == password[i + 1] for i in range(len(password) - 1)]) \
+            and all([password[i] <= password[i + 1] for i in range(len(password) - 1)])
+
+
+def p1(start, end):
+    return len([i for i in range(int(start), int(end) + 1) if criteria(str(i))])
+
+
+def criteria2(password):
+    return any([len(list(i)) == 2 for _, i in groupby(password)]) \
+            and all([password[i] <= password[i + 1] for i in range(len(password) - 1)])
+
+
+def p2(start, end):
+    return len([i for i in range(int(start), int(end) + 1) if criteria2(str(i))])
+
+
+if __name__ == "__main__":
+    _range = ('123257', '647015')
+    print("Part 1: {}".format(p1(*_range)))
+    print("Part 2: {}".format(p2(*_range)))
