1bf4b162e09ed8ec1c3d0b876ead810aaac36def
marlew <lev7@vp.pl>
2019-12-10 21:47:49 +0100

    day10 part 2

--- day10/day10.py
+++ day10/day10.py
@@ -1,18 +1,46 @@
-from itertools import groupby, product
-from math import atan2
+from itertools import groupby, cycle, islice
+from math import atan2, pi
+
 
 def parse_input(input):
-    return {(x, y) for y in range(len(input)) for x in range(len(input[y]) - 1) if input[y][x] == '#'}
-    #return [[j=='#' for j in i.strip()] for i in input]
+    return [(x, y) for y in range(len(input)) for x in range(len(input[y]) - 1) if input[y][x] == '#']
+
+
+def best_asteroid(asteroids):
+    a = [(a1, len(list(groupby(sorted([atan2(a2[1]-a1[1], a2[0]-a1[0]) for a2 in asteroids if a1 != a2]))))) for a1 in asteroids]
+    return max(a, key=lambda x: x[1])
 
 
 def p1(asteroids):
-    a = [list(groupby(sorted([atan2(a2[1]-a1[1], a2[0]-a1[0]) for a2 in asteroids if a1 != a2]))) for a1 in asteroids]
-    return max([len(x) for x in a])
+    return best_asteroid(asteroids)[1]
 
 
-def p2(input):
-    return 0
+def phase90(fun):
+    def phase90wrapper(y, x):
+        return fun(y, x) + 3*pi/2 if x < 0 and y < 0 else fun(y, x) - pi/2
+    return phase90wrapper
+
+
+@phase90
+def angle(y, x):
+    return atan2(y, x)
+
+
+def norm(a1, a2):
+    return a2[1]-a1[1], a2[0]-a1[0]
+
+
+def gen_asteroids(asteroids_groups):
+    for group in cycle(asteroids_groups):
+        if group: yield group.pop()[0]
+
+
+def p2(asteroids):
+    monitor = best_asteroid(asteroids)[0]
+    dist = lambda a, b: abs(a[0] - b[0]) + abs(a[1] - b[1])
+    others = [sorted(v, key=lambda x: dist(monitor, x[0]), reverse=True) for _, v in groupby(sorted([(a, angle(*norm(monitor, a))) for a in asteroids if monitor != a], key=lambda x: x[1]), key=lambda x: x[1])]
+    a200 = list(islice(gen_asteroids(others), 200)).pop()
+    return 100*a200[0] + a200[1]
 
 
 if __name__ == "__main__":
