bf111fa5c580dd40d1021a8347d99598aa54b560
marlew <lev7@vp.pl>
2019-12-17 18:42:17 +0100

    day 17 with manual part 2

--- day17/day17.py
+++ day17/day17.py
@@ -1,3 +1,7 @@
+from itertools import takewhile
+from enum import IntEnum
+
+
 def parse_input(input):
     return [int(i) for i in input.split(',')]
 
@@ -92,40 +96,90 @@ def run(prog, input, pos=0, rb_val=0):
 
 
 def p1(program):
+    return 0
     program = program.copy()
     program.extend([0]*5000)
     out, *_ = run(program, input=[])
-    grid = {}
-    x = y = 0
-    for o in out:
-        if o == 10:
-            y += 1
-            x = 0
-        else:
-            grid[(x, y)] = o
-            x += 1
+    grid = ''.join([chr(o) for o in out]).strip().split('\n')
 
+    print("\n".join(grid))
     
-    maxX = max(grid.keys(), key=lambda x: x[0])[0]
-    minX = min(grid.keys(), key=lambda x: x[0])[0]
-    maxY = max(grid.keys(), key=lambda x: x[1])[1]
-    minY = min(grid.keys(), key=lambda x: x[1])[1]
-    image = [[grid.get((x, y), 0) for x in range(minX, maxX+1)] for y in range(minY, maxY+1)]
-    
-    for line in image:
-        print("".join(['#' if i==35 else ('.' if i == 46 else '?') for i in line]))
-
-    result = 0
-    for pos, val in grid.items():
-        if val == 35 and all([grid.get((pos[0]+d[0], pos[1]+d[1]), 0)==35 for d in [(0,1), (1,0), (0,-1), (-1,0)]]):
-            result += pos[0] * pos[1]
+    return sum([x*y for y in range(1, len(grid) - 1) for x in range(1,len(grid[0]) - 1) if grid[y][x]=='#' and all([grid[y+d[1]][x+d[0]]=='#' for d in [(0,1), (1,0), (0,-1), (-1,0)]])])
 
 
-    return result
+class Dir(IntEnum):
+    north = 1
+    south = 2
+    west = 3
+    east = 4
 
 
-def p2(input):
-    return 0
+def get_vacuum_robot(grid):
+    for y, line in enumerate(grid):
+        for c, _dir in [('^', Dir.north), ('v', Dir.south), ('<', Dir.west), ('>', Dir.east)]:
+            x = line.find(c)
+            if x >= 0:
+                return (x, y), _dir
+
+
+def safeget(x, y, grid):
+    return grid[y][x] if 0 <= y < len(grid) and 0 <= x < len(grid[y]) else ''
+
+
+rotate = {
+    Dir.north: lambda pos, grid: ('R', Dir.east) if safeget(pos[0]+1, pos[1], grid) == "#" else (('L', Dir.west) if safeget(pos[0]-1, pos[1], grid) == "#" else (None, None)),
+    Dir.south: lambda pos, grid: ('R', Dir.west) if safeget(pos[0]-1, pos[1], grid) == "#" else (('L', Dir.east) if safeget(pos[0]+1, pos[1], grid) == "#" else (None, None)),
+    Dir.west: lambda pos, grid: ('R', Dir.north) if safeget(pos[0], pos[1]-1, grid) == "#" else (('L', Dir.south) if safeget(pos[0], pos[1]+1, grid) == "#" else (None, None)),
+    Dir.east: lambda pos, grid: ('R', Dir.south) if safeget(pos[0], pos[1]+1, grid) == "#" else (('L', Dir.north) if safeget(pos[0], pos[1]-1, grid) == "#" else (None, None))
+}
+
+
+steps_gen = {
+    Dir.north: lambda pos, _, grid_t: reversed(grid_t[pos[0]][:pos[1]]),
+    Dir.south: lambda pos, _, grid_t: grid_t[pos[0]][pos[1]+1:],
+    Dir.west: lambda pos, grid, _: reversed(grid[pos[1]][:pos[0]]),
+    Dir.east: lambda pos, grid, _: grid[pos[1]][pos[0]+1:]
+}
+
+
+update_pos = {
+    Dir.north: lambda pos, steps: (pos[0], pos[1] - steps),
+    Dir.south: lambda pos, steps: (pos[0], pos[1] + steps),
+    Dir.west: lambda pos, steps: (pos[0] - steps, pos[1]),
+    Dir.east: lambda pos, steps: (pos[0] + steps, pos[1])
+}
+
+def p2(program):
+    program1 = program.copy()
+    #program[0] = 2
+    program1.extend([0]*5000)
+    out, *_ = run(program1, input=[])
+    grid = ''.join([chr(o) for o in out]).strip().split('\n')
+    grid_t = [i for i in zip(*grid)]
+
+    #print("\n".join(grid))
+    #print()
+    #print("\n".join(["".join(g) for g in grid_t]))
+
+    vrpos, vrdir = get_vacuum_robot(grid)
+    path = []
+    rot, vrdir = rotate[vrdir](vrpos, grid)
+    while rot:
+        path.append(rot)
+        steps = len(list(takewhile(lambda x: x == '#', steps_gen[vrdir](vrpos, grid, grid_t))))
+        path.append(str(steps))
+        vrpos = update_pos[vrdir](vrpos, steps)
+        rot, vrdir = rotate[vrdir](vrpos, grid)
+
+    #print(",".join(path))
+
+    program2 = program.copy()
+    program2[0] = 2
+    program2.extend([0]*5000)
+    inp=[ord(i) for i in reversed("A,A,B,C,B,C,B,C,B,A\nL,10,L,8,R,8,L,8,R,6\nR,6,R,8,R,8\nR,6,R,6,L,8,L,10\ny\n")]
+    print(inp)
+    out, *_ = run(program2, input=inp)
+    return out[-1]
 
 
 if __name__ == "__main__":
